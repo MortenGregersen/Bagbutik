ccf4ae0dd7d84ce7a7ca7183abd292e5d8111ca5
Morten Bjerg Gregersen <morten@mogee.dk>
2021-02-07 22:02:36 +0100

    Package.swift clean up

--- Package.swift
+++ Package.swift
@@ -17,36 +17,35 @@ let package = Package(
             targets: ["BagbutikCLI"])
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-argument-parser", from: "0.3.1"),
+        .package(url: "https://github.com/apple/swift-argument-parser", from: "0.3.2"),
         .package(url: "https://github.com/SwiftGen/StencilSwiftKit", from: "2.7.2"),
-        .package(url: "https://github.com/nicklockwood/SwiftFormat", from: "0.46.2"),
+        .package(url: "https://github.com/nicklockwood/SwiftFormat", from: "0.47.11"),
         .package(url: "https://github.com/apple/swift-crypto", from: "1.1.3")
     ],
     targets: [
         .target(
             name: "Bagbutik",
             dependencies: [.product(name: "Crypto", package: "swift-crypto")]),
+        .testTarget(
+            name: "BagbutikTests",
+            dependencies: ["Bagbutik", .product(name: "Crypto", package: "swift-crypto")]),
         .target(
             name: "BagbutikCLI",
             dependencies: [
                 "Bagbutik",
-                "BagbutikSpecDecoder",
                 "BagbutikGenerator",
                 .product(name: "ArgumentParser", package: "swift-argument-parser")
             ]),
         .target(
             name: "BagbutikGenerator",
             dependencies: ["BagbutikSpecDecoder", "StencilSwiftKit", "SwiftFormat"]),
+        .testTarget(
+            name: "BagbutikGeneratorTests",
+            dependencies: ["BagbutikGenerator"]),
         .target(
             name: "BagbutikSpecDecoder"),
         .testTarget(
-            name: "BagbutikTests",
-            dependencies: ["Bagbutik", .product(name: "Crypto", package: "swift-crypto")]),
-        .testTarget(
-            name: "BagbutikGeneratorTests",
-            dependencies: ["BagbutikGenerator"],
-            resources: [.copy("app-store-connect-openapi-spec.json")]),
-        .testTarget(
             name: "BagbutikSpecDecoderTests",
             dependencies: ["BagbutikSpecDecoder"])
+
     ])
