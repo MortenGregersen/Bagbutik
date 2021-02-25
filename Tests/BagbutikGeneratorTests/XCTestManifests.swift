61868f95f9dfefd9e5b1e5ac8bd162b5979ec4ee
Morten Bjerg Gregersen <morten@mogee.dk>
2021-02-25 23:44:36 +0100

    Update XCTestManifests

--- Tests/BagbutikGeneratorTests/XCTestManifests.swift
+++ Tests/BagbutikGeneratorTests/XCTestManifests.swift
@@ -3,7 +3,13 @@ import XCTest
 #if !canImport(ObjectiveC)
 public func allTests() -> [XCTestCaseEntry] {
     return [
-        testCase(BagbutikGeneratorTests.allTests),
+        testCase(AttributesSchemaRendererTests.allTests),
+        testCase(EnumSchemaRendererTests.allTests),
+        testCase(ObjectSchemaRendererTests.allTests),
+        testCase(OneOfSchemaRendererTests.allTests),
+        testCase(OperationRendererTests.allTests),
+        testCase(PropertyRendererTests.allTests),
+        testCase(GeneratorTests.allTests),
     ]
 }
 #endif
--- Tests/BagbutikSpecDecoderTests/XCTestManifests.swift
+++ Tests/BagbutikSpecDecoderTests/XCTestManifests.swift
@@ -3,7 +3,18 @@ import XCTest
 #if !canImport(ObjectiveC)
 public func allTests() -> [XCTestCaseEntry] {
     return [
-        testCase(BagbutikSpecDecoderTests.allTests),
+        testCase(DynamicCodingKeysTests.allTests),
+        testCase(StringCasingTests.allTests),
+        testCase(OperationTests.allTests),
+        testCase(ParameterTests.allTests),
+        testCase(PathTests.allTests),
+        testCase(EnumCaseTests.allTests),
+        testCase(EnumSchemaTests.allTests),
+        testCase(ObjectSchemaTests.allTests),
+        testCase(OneOfOptionTests.allTests),
+        testCase(PropertyTypeTests.allTests),
+        testCase(SchemaTests.allTests),
+        testCase(SimplePropertyTypeTests.allTests),
     ]
 }
 #endif
