21dadb4f8c0b4e1fee3d8b43db0c8a3d8b8176c0
Morten Bjerg Gregersen <morten@mogee.dk>
2021-03-25 23:20:58 +0100

    Documentation for EnumSchema

--- Sources/BagbutikSpecDecoder/Schemas/EnumSchema.swift
+++ Sources/BagbutikSpecDecoder/Schemas/EnumSchema.swift
@@ -1,26 +1,41 @@
 import Foundation
 
+/// A representation of an enum
 public struct EnumSchema: Decodable, Equatable {
+    /// The name of the enum
     public let name: String
+    /// The type of the value of the enum's cases
     public let type: String
+    /// The documentation for the enum - if any
     public let documentation: Schema.Documentation?
+    /// The enum's cases
     public let cases: [EnumCase]
 
-    enum CodingKeys: String, CodingKey {
+    private enum CodingKeys: String, CodingKey {
         case type
         case `enum`
     }
 
-    public init(name: String, type: String, values: [String]) {
-        self.init(name: name, type: type, values: values, lookupDocumentation: Schema.Documentation.lookupDocumentation)
+    /**
+     Initialize a new enum
+
+     - Parameter name: The name of the enum
+     - Parameter type: The type of the value of the enum's cases
+     - Parameter caseValues: Values for the enum's cases
+     */
+    public init(name: String, type: String, caseValues: [String]) {
+        self.init(name: name,
+                  type: type,
+                  caseValues: caseValues,
+                  lookupDocumentation: Schema.Documentation.lookupDocumentation)
     }
-    
-    internal init(name: String, type: String, values: [String], lookupDocumentation: (String) -> Schema.Documentation?) {
+
+    internal init(name: String, type: String, caseValues: [String], lookupDocumentation: (String) -> Schema.Documentation?) {
         self.type = type
         self.name = name
         let documentation = lookupDocumentation(name)
         self.documentation = documentation
-        let cases = values.map {
+        let cases = caseValues.map {
             EnumCase(id: $0.camelCased(with: "_"), value: $0, documentation: documentation?.properties[$0])
         }
         if name == "BundleIdPlatform" {
