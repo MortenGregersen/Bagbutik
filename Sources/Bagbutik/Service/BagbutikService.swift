eacd51089b3359c8bc3ad23f163bc40f9f3cf64f
Morten Bjerg Gregersen <morten@mogee.dk>
2022-02-27 21:19:03 +0100

    Update BagbutikService.swift

--- Sources/Bagbutik/Service/BagbutikService.swift
+++ Sources/Bagbutik/Service/BagbutikService.swift
@@ -3,6 +3,20 @@ import Foundation
 #if canImport(FoundationNetworking)
     // Linux support
     import FoundationNetworking
+    extension URLSession {
+        func data(for request: URLRequest, delegate: URLSessionTaskDelegate?) async throws -> (Data, URLResponse) {
+            return try await withCheckedThrowingContinuation { continuation in
+                let task = dataTask(with: request) { data, response, error in
+                    if let data = data, let response = response {
+                        continuation.resume(returning: (data, response))
+                    } else {
+                        continuation.resume(throwing: error!)
+                    }
+                }
+                task.resume()
+            }
+        }
+    }
 #endif
 
 public protocol BagbutikServiceProtocol {
