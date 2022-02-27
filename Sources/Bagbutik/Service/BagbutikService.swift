488817a36e796c5717cf7f65ab675d3924f8d427
Morten Bjerg Gregersen <morten@mogee.dk>
2022-02-27 22:48:09 +0100

    Use CryptoKit on Mac

--- Sources/Bagbutik/Service/BagbutikService.swift
+++ Sources/Bagbutik/Service/BagbutikService.swift
@@ -1,23 +1,27 @@
+#if canImport(CryptoKit)
+import CryptoKit
+#else
 import Crypto
+#endif
 import Foundation
 #if canImport(FoundationNetworking)
-    // Linux support
-    import FoundationNetworking
-    public extension URLSession {
-        // URLSession in FoundationNetworking doesn't have async/await versions of the dataTask functions
-        func data(for request: URLRequest, delegate: URLSessionTaskDelegate?) async throws -> (Data, URLResponse) {
-            return try await withCheckedThrowingContinuation { continuation in
-                let task = dataTask(with: request) { data, response, error in
-                    if let data = data, let response = response {
-                        continuation.resume(returning: (data, response))
-                    } else {
-                        continuation.resume(throwing: error!)
-                    }
+// Linux support
+import FoundationNetworking
+public extension URLSession {
+    // URLSession in FoundationNetworking doesn't have async/await versions of the dataTask functions
+    func data(for request: URLRequest, delegate: URLSessionTaskDelegate?) async throws -> (Data, URLResponse) {
+        return try await withCheckedThrowingContinuation { continuation in
+            let task = dataTask(with: request) { data, response, error in
+                if let data = data, let response = response {
+                    continuation.resume(returning: (data, response))
+                } else {
+                    continuation.resume(throwing: error!)
                 }
-                task.resume()
             }
+            task.resume()
         }
     }
+}
 #endif
 
 public protocol BagbutikServiceProtocol {
