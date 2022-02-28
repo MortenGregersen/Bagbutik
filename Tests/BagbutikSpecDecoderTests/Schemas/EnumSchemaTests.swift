1450c2c0c0f2ebe539c6886974510722c383aa47
Morten Bjerg Gregersen <morten@mogee.dk>
2022-02-28 20:54:01 +0100

    More tests (#49)
    * Add more tests
    
    * Automatic handling if FB8977648 is fixed
    
    * Ignore dumb getters

--- Tests/BagbutikSpecDecoderTests/Schemas/EnumSchemaTests.swift
+++ Tests/BagbutikSpecDecoderTests/Schemas/EnumSchemaTests.swift
@@ -52,6 +52,25 @@ final class EnumSchemaTests: XCTestCase {
         ])
         XCTAssertEqual(enumSchema.name, "BundleIdPlatform")
     }
+    
+    func testDecodableInitFB8977648_Fixed() throws {
+        let jsonString = """
+        {
+            "BundleIdPlatform" : {
+                "type" : "string",
+                "enum" : [ "IOS", "MAC_OS", "UNIVERSAL" ]
+            }
+        }
+        """
+        let enumSchema = try JSONDecoder().decode([String: EnumSchema].self, from: jsonString.data(using: .utf8)!).values.first!
+        XCTAssertEqual(enumSchema.type, "string")
+        XCTAssertEqual(enumSchema.cases, [
+            EnumCase(id: "iOS", value: "IOS", documentation: "A string that represents iOS."),
+            EnumCase(id: "macOS", value: "MAC_OS", documentation: "A string that represents macOS."),
+            EnumCase(id: "universal", value: "UNIVERSAL", documentation: nil),
+        ])
+        XCTAssertEqual(enumSchema.name, "BundleIdPlatform")
+    }
 
     func testDecodablePropertyNamedType() throws {
         let jsonString = """
@@ -69,6 +88,24 @@ final class EnumSchemaTests: XCTestCase {
         """
         let objectSchema = try JSONDecoder().decode([String: ObjectSchema].self, from: jsonString.data(using: .utf8)!).values.first!
         guard case .enumSchema(let enumSchema) = objectSchema.properties["type"]!.type else { XCTFail("Wrong type"); return }
+        XCTAssertEqual(enumSchema.name, "PhoneNumberType")
+    }
+    
+    func testDecodablePropertyNamedType_Linux() throws {
+        let jsonString = """
+        {
+            "phoneNumber" : {
+                "properties" : {
+                    "type" : {
+                        "type" : "string",
+                        "enum" : [ "FAX", "LANDLINE", "MOBILE", "TOLLFREE" ]
+                    }
+                }
+            }
+        }
+        """
+        let decodedDictionary = try JSONDecoder().decode([String: [String: [String: EnumSchema]]].self, from: jsonString.data(using: .utf8)!)
+        let enumSchema = decodedDictionary["phoneNumber"]!["properties"]!["type"]!
         XCTAssertEqual(enumSchema.name, "PhoneNumberType")
     }
 }
