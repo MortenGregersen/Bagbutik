59f45d429dfecac620ef8845a06fe15f82c07a89
Morten Bjerg Gregersen <morten@mogee.dk>
2022-03-03 22:28:50 +0100

    Create pull request

--- check-spec-version.sh
+++ check-spec-version.sh
@@ -9,7 +9,7 @@ fi
 if [[ $download_newest_spec_output =~ Spec\ file\ downloaded\ to\ (.+) ]]; then
     spec_file_path=${BASH_REMATCH[1]}
 else
-    echo "No path to a spec file found in output from bagbutik CLI"
+    echo "No path to a spec file found in output from Bagbutik CLI"
     exit 42
 fi
 
@@ -23,9 +23,18 @@ if [ "$current_version" == "$downloaded_version" ]; then
     exit 0
 else
     echo "New spec is found. Will generate new endpoints and models."
-    swift run bagbutik generate --spec-path $spec_file_path
 fi
 
+swift run bagbutik generate --spec-path $spec_file_path
 rm $spec_file_path
+echo $downloaded_version > spec-version
 
-
+git branch spec-$downloaded_version
+git add .
+git commit -m "Update from new spec ($downloaded_version)"
+create_pr_output=$(gh pr create --fill)
+if [ $? != 0 ]; then
+    exit $?
+else
+    echo "Pull request created: $create_pr_output"
+fi
