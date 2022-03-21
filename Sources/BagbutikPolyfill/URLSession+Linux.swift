2ea488dd068f404a753fbd52c714bece17395ed8
Morten Bjerg Gregersen <morten@mogee.dk>
2022-03-21 21:35:05 +0100

    Add async download function to URLSession on Linux

--- Sources/BagbutikPolyfill/URLSession+Linux.swift
+++ Sources/BagbutikPolyfill/URLSession+Linux.swift
@@ -4,10 +4,24 @@ import Foundation
 import FoundationNetworking
 
 public extension URLSession {
-    // URLSession in FoundationNetworking doesn't have async/await versions of the dataTask functions yet
+    // URLSession in FoundationNetworking doesn't have async/await versions of the dataTask and downloadTask functions yet
+    
     func data(for request: URLRequest, delegate: URLSessionTaskDelegate?) async throws -> (Data, URLResponse) {
         return try await withCheckedThrowingContinuation { continuation in
             let task = dataTask(with: request) { data, response, error in
+                if let data = data, let response = response {
+                    continuation.resume(returning: (data, response))
+                } else {
+                    continuation.resume(throwing: error!)
+                }
+            }
+            task.resume()
+        }
+    }
+    
+    func download(from url: URL, delegate: URLSessionTaskDelegate? = nil) async throws -> (URL, URLResponse) {
+        return try await withCheckedThrowingContinuation { continuation in
+            let task = downloadTask(with: url) { data, response, error in
                 if let data = data, let response = response {
                     continuation.resume(returning: (data, response))
                 } else {
