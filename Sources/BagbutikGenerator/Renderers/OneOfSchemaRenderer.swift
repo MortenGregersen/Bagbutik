d1ad62b759da7f1ae08e189ee693d0df45b9b1a8
Morten Bjerg Gregersen <morten@mogee.dk>
2022-07-25 23:03:23 +0200

    OneOfSchemaRenderer without Stencil

--- Sources/BagbutikGenerator/Renderers/OneOfSchemaRenderer.swift
+++ Sources/BagbutikGenerator/Renderers/OneOfSchemaRenderer.swift
@@ -15,54 +15,56 @@ public class OneOfSchemaRenderer {
      - Returns: The rendered one of schema
      */
     public func render(name: String, oneOfSchema: OneOfSchema) throws -> String {
-        let context = Self.oneOfContext(for: oneOfSchema, named: name)
-        let rendered = try environment.renderTemplate(name: "oneOfTemplate", context: context)
-        return try SwiftFormat.format(rendered)
-    }
+        var rendered = "public enum \(name): Codable {\n"
+        let options = oneOfSchema.options
+            .map { EnumCase(id: $0.schemaName.lowercasedFirstLetter(), value: $0.schemaName) }
+            .sorted { $0.id < $1.id }
+        options.forEach {
+            rendered += "case \($0.id)(\($0.value))\n"
+        }
+        rendered += "\npublic init(from decoder: Decoder) throws {\n"
+        options.enumerated().forEach {
+            let option = $0.element
+            var renderedOption = """
+            if let \(option.id) = try? \(option.value)(from: decoder) {
+                self = .\(option.id)(\(option.id))
 
-    private let environment = Environment(loader: DictionaryLoader(templates: ["oneOfTemplate":
             """
-            public enum {{ name }}: Codable {
-                {% for option in options %}
-                case {{ option.id }}({{ option.value }}){%
-                endfor %}
+            if $0.offset != 0 {
+                renderedOption = "} else " + renderedOption
+            }
+            rendered += renderedOption
+        }
+        rendered += """
+            } else {
+                throw DecodingError.typeMismatch(\(name).self, DecodingError.Context(codingPath: decoder.codingPath,
+                                                                                     debugDescription: "Unknown \(name)"))
+            }
+        }
 
-                public init(from decoder: Decoder) throws {
-                    {% for option in options %}
-                        {% if forloop.first %}
-                        if let {{ option.id }} = try? {{ option.value }}(from: decoder) {
-                            self = .{{ option.id }}({{ option.id }}){%
-                        else %} } else if let {{ option.id }} = try? {{ option.value }}(from: decoder) {
-                            self = .{{ option.id }}({{ option.id }}){%
-                        endif %}{%
-                    endfor %}
-                    } else {
-                        throw DecodingError.typeMismatch({{ name }}.self, DecodingError.Context(codingPath: decoder.codingPath,
-                                                                                            debugDescription: "Unknown {{ name  }}"))
-                    }
-                }
+        """
+        rendered += """
 
-                public func encode(to encoder: Encoder) throws {
-                    switch self {
-                    {% for option in options %}
-                    case .{{ option.id }}(let value):
-                        try value.encode(to: encoder){%
-                    endfor %}
-                    }
-                }
+        public func encode(to encoder: Encoder) throws {
+            switch self {
 
-                private enum CodingKeys: String, CodingKey {
-                    case type
+        """
+        options.forEach {
+            rendered += """
+            case .\($0.id)(let value):
+                try value.encode(to: encoder)
+
+            """
+        }
+        rendered += """
                 }
             }
-            """]), extensions: StencilSwiftKit.stencilSwiftEnvironment().extensions)
 
-    internal static func oneOfContext(for oneOfSchema: OneOfSchema, named name: String) -> [String: Any] {
-        return [
-            "name": name,
-            "options": oneOfSchema.options
-                .map { EnumCase(id: $0.schemaName.lowercasedFirstLetter(), value: $0.schemaName) }
-                .sorted { $0.id < $1.id },
-        ]
+            private enum CodingKeys: String, CodingKey {
+                case type
+            }
+        }
+        """
+        return try SwiftFormat.format(rendered)
     }
 }
