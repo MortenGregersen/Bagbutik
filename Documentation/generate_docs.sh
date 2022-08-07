4fcfb01b7a70ec0fd2fe9a9621d80367f196801b
Morten Bjerg Gregersen <morten@mogee.dk>
2022-08-07 23:43:46 +0200

    Information about the different libraries in README

--- Documentation/generate_docs.sh
+++ Documentation/generate_docs.sh
@@ -6,5 +6,6 @@ set -e
 rm -rf .build
 rm -rf docs
 jazzy
-sed -i'' -e 's/Bagbutik_Core/Bagbutik/' docs/index.html
+sed -i'' -e 's/Bagbutik_Core Reference/Bagbutik Reference/' docs/index.html
+sed -i'' -e 's/Bagbutik_Core  Reference/Bagbutik  Reference/' docs/index.html
 open docs/index.html
