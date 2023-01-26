a765f69ed5a86757920b9d774f3a7c9b4853fdc8
Morten Bjerg Gregersen <759680+MortenGregersen@users.noreply.github.com>
2023-01-26 11:53:13 +0100

    Fix tests

--- Tests/BagbutikGeneratorTests/Renderers/ObjectSchemaRendererTests.swift
+++ Tests/BagbutikGeneratorTests/Renderers/ObjectSchemaRendererTests.swift
@@ -180,7 +180,7 @@ final class ObjectSchemaRendererTests: XCTestCase {
 
             public init(from decoder: Decoder) throws {
                 let container = try decoder.container(keyedBy: CodingKeys.self)
-                _data = try container.decode(NullCodable<Data>.self, forKey: .data)
+                data = try container.decode(Data.self, forKey: .data)
             }
 
             public func encode(to encoder: Encoder) throws {
@@ -1036,7 +1036,7 @@ final class ObjectSchemaRendererTests: XCTestCase {
 
                     public init(from decoder: Decoder) throws {
                         let container = try decoder.container(keyedBy: CodingKeys.self)
-                        _data = try container.decode(NullCodable<Data>.self, forKey: .data)
+                        data = try container.decode(Data.self, forKey: .data)
                     }
 
                     public func encode(to encoder: Encoder) throws {
