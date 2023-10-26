e1bdc2dd997cc3162ee636543623a2d503cc9574
Morten Bjerg Gregersen <morten@coolyellowowl.com>
2023-10-26 20:10:37 +0200

    Use macOS 13 when generating docs (#144)
    * Update docs.yml
    
    * Use Gemfile for jazzy
    
    * Install gems
    
    * Update Gemfile.lock
    
    * Update docs.yml
    
    ---------
    
    Co-authored-by: Morten Bjerg Gregersen <759680+MortenGregersen@users.noreply.github.com>

--- Documentation/generate_docs.sh
+++ Documentation/generate_docs.sh
@@ -5,7 +5,8 @@ set -e
 
 rm -rf .build
 rm -rf docs
-jazzy
+bundle install
+bundle exec jazzy
 sed -i'' -e 's/Bagbutik_Core Reference/Bagbutik Reference/' docs/index.html
 sed -i'' -e 's/Bagbutik_Core  Reference/Bagbutik  Reference/' docs/index.html
 open docs/index.html
