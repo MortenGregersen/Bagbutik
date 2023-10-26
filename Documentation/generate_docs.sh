25c83b3e3f87f0610d7d4b4ad188e3b3c38fb3eb
Morten Bjerg Gregersen <759680+MortenGregersen@users.noreply.github.com>
2023-10-26 11:07:24 +0200

    Install gems

--- Documentation/generate_docs.sh
+++ Documentation/generate_docs.sh
@@ -5,6 +5,7 @@ set -e
 
 rm -rf .build
 rm -rf docs
+bundle install
 bundle exec jazzy
 sed -i'' -e 's/Bagbutik_Core Reference/Bagbutik Reference/' docs/index.html
 sed -i'' -e 's/Bagbutik_Core  Reference/Bagbutik  Reference/' docs/index.html
