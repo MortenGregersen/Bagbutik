977c8320bef0a770018a168913af686b788f845f
Morten Bjerg Gregersen <morten@coolyellowowl.com>
2024-01-17 13:11:00 +0100

    Add "Generate individual keys" user role (#149)
    * Add GENERATE_INDIVIDUAL_KEYS user role
    
    * Regenerate
    
    ---------
    
    Co-authored-by: Morten Bjerg Gregersen <759680+MortenGregersen@users.noreply.github.com>

--- Sources/Bagbutik-Models/Extensions/UserRole+PrettyName.swift
+++ Sources/Bagbutik-Models/Extensions/UserRole+PrettyName.swift
@@ -2,20 +2,21 @@ public extension UserRole {
     /// A pretty name for the case. The names are added as best effort, and a better name could exist.
     var prettyName: String {
         switch self {
-        case .admin: return "Admin"
-        case .finance: return "Finance"
-        case .accessToReports: return "Access to Reports"
-        case .sales: return "Sales"
-        case .developer: return "Developer"
-        case .appManager: return "App Manager"
-        case .customerSupport: return "Customer Support"
-        case .marketing: return "Marketing"
-        case .accountHolder: return "Account Holder"
+        case .admin: "Admin"
+        case .finance: "Finance"
+        case .accessToReports: "Access to Reports"
+        case .sales: "Sales"
+        case .developer: "Developer"
+        case .appManager: "App Manager"
+        case .customerSupport: "Customer Support"
+        case .marketing: "Marketing"
+        case .accountHolder: "Account Holder"
         // Not mentioned in documentation: https://developer.apple.com/support/roles
-        case .createApps: return "Create Apps"
-        case .cloudManagedDeveloperId: return "Cloud Managed Developer ID"
-        case .cloudManagedAppDistribution: return "Cloud Managed App Distribution"
-        case .imageManager: return "Image Manager"
+        case .createApps: "Create Apps"
+        case .cloudManagedDeveloperId: "Cloud Managed Developer ID"
+        case .cloudManagedAppDistribution: "Cloud Managed App Distribution"
+        case .imageManager: "Image Manager"
+        case .generateIndividualKeys: "Generate Individual Keys"
         }
     }
 }
