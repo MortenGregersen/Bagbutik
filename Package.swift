7d1e40ec9245e3e510a386b3d767d55a4e4c51ff
Morten Bjerg Gregersen <morten@atterdagapps.com>
2024-10-04 23:01:50 +0200

    Update dependencies (#207)

--- Package.swift
+++ Package.swift
@@ -63,9 +63,9 @@ let package = Package(
             targets: ["BagbutikCLI"])
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.3.0"),
-        .package(url: "https://github.com/nicklockwood/SwiftFormat", from: "0.53.0"),
-        .package(url: "https://github.com/apple/swift-crypto", from: "3.2.0"),
+        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.5.0"),
+        .package(url: "https://github.com/nicklockwood/SwiftFormat", from: "0.54.5"),
+        .package(url: "https://github.com/apple/swift-crypto", from: "3.8.0"),
     ],
     targets: [
         .target(name: "Bagbutik-Core", dependencies: [
