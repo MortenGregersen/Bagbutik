716b1ecd85ca81779e3a8f3669574272f06e1a36
Morten Bjerg Gregersen <morten@atterdagapps.com>
2024-12-09 14:06:07 +0100

    Add tests

--- Tests/Bagbutik-ModelsTests/Extensions/AppMediaStateError+MoreInfoTests.swift
+++ Tests/Bagbutik-ModelsTests/Extensions/AppMediaStateError+MoreInfoTests.swift
@@ -7,6 +7,21 @@ final class AppMediaStateErrorMoreInfoTests: XCTestCase {
         XCTAssertNotEqual(error.betterDescription, error.description)
     }
     
+    func testBetterDescription_MovieResaveStereo() {
+        let error = AppMediaStateError(code: "MOV_RESAVE_STEREO", description: "MOV_RESAVE_STEREO")
+        XCTAssertNotEqual(error.betterDescription, error.description)
+    }
+    
+    func testBetterDescription_MovieResaveLonger() {
+        let error = AppMediaStateError(code: "MOV_RESAVE_LONGER", description: "MOV_RESAVE_LONGER")
+        XCTAssertNotEqual(error.betterDescription, error.description)
+    }
+    
+    func testBetterDescription_MovieResaveTrim() {
+        let error = AppMediaStateError(code: "MOV_RESAVE_TRIM", description: "MOV_RESAVE_TRIM")
+        XCTAssertNotEqual(error.betterDescription, error.description)
+    }
+    
     func testBetterDescription_Unknown() {
         let error = AppMediaStateError(code: "SOME_ERROR", description: "SOME_ERROR")
         XCTAssertEqual(error.betterDescription, error.description)
@@ -14,6 +29,21 @@ final class AppMediaStateErrorMoreInfoTests: XCTestCase {
     
     func testLearnMoreUrl_ImageIncorrectDimensions() {
         let error = AppMediaStateError(code: "IMAGE_INCORRECT_DIMENSIONS", description: "IMAGE_INCORRECT_DIMENSIONS")
+        XCTAssertNotNil(error.learnMoreUrl)
+    }
+    
+    func testLearnMoreUrl_MovieResaveStereo() {
+        let error = AppMediaStateError(code: "MOV_RESAVE_STEREO", description: "MOV_RESAVE_STEREO")
+        XCTAssertNotNil(error.learnMoreUrl)
+    }
+    
+    func testLearnMoreUrl_MovieResaveLonger() {
+        let error = AppMediaStateError(code: "MOV_RESAVE_LONGER", description: "MOV_RESAVE_LONGER")
+        XCTAssertNotNil(error.learnMoreUrl)
+    }
+    
+    func testLearnMoreUrl_MovieResaveTrim() {
+        let error = AppMediaStateError(code: "MOV_RESAVE_TRIM", description: "MOV_RESAVE_TRIM")
         XCTAssertNotNil(error.learnMoreUrl)
     }
     
