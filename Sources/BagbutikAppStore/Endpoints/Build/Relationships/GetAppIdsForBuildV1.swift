b971fb4a882a4eb89a6b0954f336d08784a70c8f
Morten Bjerg Gregersen <morten@atterdagapps.com>
2026-08-11 15:27:24 +0200

    Restore generated Build endpoints (#335)

---
+++ Sources/BagbutikAppStore/Endpoints/Build/Relationships/GetAppIdsForBuildV1.swift
@@ -0,0 +1,20 @@
+import BagbutikCore
+import BagbutikAppStoreModels
+
+public extension Request {
+    /**
+     # Read the app id of a build
+     Get the app ID for a specific build.
+
+     Full documentation:
+     <https://developer.apple.com/documentation/appstoreconnectapi/get-v1-builds-_id_-relationships-app>
+
+     - Parameter id: The id of the requested resource
+     - Returns: A ``Request`` to send to an instance of ``BagbutikService``
+     */
+    static func getAppIdsForBuildV1(id: String) -> Request<BuildAppLinkageResponse, ErrorResponse> {
+        .init(
+            path: "/v1/builds/\(id)/relationships/app",
+            method: .get)
+    }
+}
---
+++ Sources/BagbutikTestFlight/Endpoints/Build/Relationships/GetPreReleaseVersionIdsForBuildV1.swift
@@ -0,0 +1,19 @@
+import BagbutikCore
+import BagbutikTestFlightModels
+
+public extension Request {
+    /**
+     # Get the prerelease version ID for a build
+
+     Full documentation:
+     <https://developer.apple.com/documentation/appstoreconnectapi/get-v1-builds-_id_-relationships-preReleaseVersion>
+
+     - Parameter id: The id of the requested resource
+     - Returns: A ``Request`` to send to an instance of ``BagbutikService``
+     */
+    static func getPreReleaseVersionIdsForBuildV1(id: String) -> Request<BuildPreReleaseVersionLinkageResponse, ErrorResponse> {
+        .init(
+            path: "/v1/builds/\(id)/relationships/preReleaseVersion",
+            method: .get)
+    }
+}
